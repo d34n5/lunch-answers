063fb73345e88f0ce9bbc2df3fe719cdb3c8ebac
Dean Scelza <deanscelza@gmail.com>
2016-10-28 09:03:42 -0700

    create question model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
+var questions = [{
+  content: "Does anyone know of any Monday lunch specials?",
+  author: "CartSeeker",
+  notes: "Loves Asian street food"
+}, {
+  content: "Biggest Burritos in Downtown?",
+  author: "BurritoManiac",
+  notes: "I live for Taco Tuesday"
+}, {
+  content: "Best $5 sandwich",
+  author: "SueEats",
+  notes: "Nothing says I've given up like a Subway sandwich"
+}, {
+  content: "Most authentic Thai cart?",
+  author: "CartSeeker",
+  notes: "Loves Asian street food"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
