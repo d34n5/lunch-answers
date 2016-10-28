e6181f83f2ea6193b2f63bea36df0f775054bf5d
Dean Scelza <deanscelza@gmail.com>
2016-10-28 12:01:27 -0700

    refactor question templates and routes

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,16 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isNoteShowing: false,
-  updateQuestionForm: false,
   actions: {
     noteShow: function() {
       this.set('isNoteShowing', true);
     },
     noteHide: function() {
       this.set('isNoteShowing', false);
-    },
-    update(question, params) {
-      this.sendAction('update', question, params);
     }
   }
 });
