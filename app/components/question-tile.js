a389008c45a7509a2c010076d45791a9cae79030
Dean Scelza <deanscelza@gmail.com>
2016-10-28 10:55:50 -0700

    add edit question functionality

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isNoteShowing: false,
+  updateQuestionForm: false,
   actions: {
     noteShow: function() {
       this.set('isNoteShowing', true);
     },
     noteHide: function() {
       this.set('isNoteShowing', false);
+    },
+    update(question, params) {
+      this.sendAction('update', question, params);
     }
   }
 });
