62795cf50da117fe8a4ca34868037d403d3b06a1
Dean Scelza <deanscelza@gmail.com>
2016-11-04 14:52:18 -0700

    implement unanswered question helper

---
+++ tests/unit/helpers/question-unanswered-test.js
@@ -0,0 +1,12 @@
+
+import { questionUnanswered } from 'lunch-answers/helpers/question-unanswered';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question unanswered');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionUnanswered([42]);
+  assert.ok(result);
+});
+
