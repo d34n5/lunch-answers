10a1bb02e8b1f323aa2d0c40d9e36f3497a8c43a
Dean Scelza <deanscelza@gmail.com>
2016-11-05 14:57:51 -0700

    display favorites in separate column

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoritesList: Ember.inject.service(),
-  sortBy: ['rating:desc'],
-  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy')
+  sortBy: ["question.get('answers').get('length'):desc"],
+  sortedQuestions: Ember.computed.sort('question.answers', 'sortBy'),
 });
