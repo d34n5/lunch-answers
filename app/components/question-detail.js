448defbc474b9f22f43e124e7333b4f78b711999
Dean Scelza <deanscelza@gmail.com>
2016-11-05 07:56:13 -0700

    inject favorites list onto index page

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoritesList: Ember.inject.service(),
   sortBy: ['rating:desc'],
   sortedAnswers: Ember.computed.sort('question.answers', 'sortBy')
 });
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,21 +1,25 @@
 import Ember from 'ember';
 
+// order of operations an issue??
+// Feel like problem is in this component
+
 export default Ember.Component.extend({
+  favoritesList: Ember.inject.service(),
   isNoteShowing: false,
   // updateQuestionForm: false,
   actions: {
     addToFavorites(question) {
       this.get('favoritesList').add(question);
-      // this.set('addedToFavorites', true);
+      this.set('addedToFavorites', true);
     },
     noteShow() {
       this.set('isNoteShowing', true);
     },
     noteHide() {
       this.set('isNoteShowing', false);
-    // },
+     }
     // update (question, params) {
     //   this.sendAction('update', question, params);
-    }
+  //}
   }
 });
